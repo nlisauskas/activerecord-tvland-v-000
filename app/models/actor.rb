c93247a0f53ba197fd5049164bcfd064b712f184
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-12-18 23:24:02 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,12 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def self.full_name
+    first = self.first_name
+    last = self.last_name
+    join(first," ",last)
+  end
+
+
 end
