0156dbf9ecbfc90979605237aceaeb7a3f02dd52
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-12-19 00:04:01 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 8) do
+ActiveRecord::Schema.define(version: 9) do
 
   create_table "actors", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
+    t.string  "first_name"
+    t.string  "last_name"
+    t.integer "show_id"
   end
 
   create_table "characters", force: :cascade do |t|
